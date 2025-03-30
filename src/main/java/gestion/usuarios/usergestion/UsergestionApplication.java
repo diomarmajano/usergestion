e3e9f186455b0f21a0deb0a77270dd3603ed07ac
Diomar Majano <http.diomar@gmail.com>
2025-03-30 20:49:02 -0300

    creacion del proyecto

---
+++ src/main/java/gestion/usuarios/usergestion/UsergestionApplication.java
@@ -0,0 +1,13 @@
+package gestion.usuarios.usergestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsergestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsergestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestion/usuarios/usergestion/UsergestionApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion.usuarios.usergestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsergestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
